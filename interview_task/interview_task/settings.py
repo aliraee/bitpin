ed5061a1d17bb6a010500d512ae1e3d6e7d1c5a0
root <aliraee1@yahoo.com>
2024-07-07 13:18:48 +0100

    add dev setting

--- interview_task/interview_task/settings.py
+++ interview_task/interview_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4(wwksd_n=7su+)@yj!098=m8n$wh$zgb@o1m%f2r%+=2kjgs^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
